6ebc42b7c15aadcd559e9af231e5c607a887bb71
Agiel <agiel.negura@gmail.com>
2024-12-22 12:18:18 +0100

    Day 22 - Faster Part 2

--- day-22/src/bin/part2.rs
+++ day-22/src/bin/part2.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, BTreeSet};
 
 use aocd::*;
 
@@ -18,46 +18,34 @@ fn next(input: u64) -> u64 {
 
 fn solve(input: &str) -> i32 {
     let secrets = input.lines().map(|s| s.parse().unwrap());
-    let change_maps = secrets
-        .map(|s| {
-            let mut change_map = BTreeMap::new();
-            let mut s = s;
-            let mut a = 0;
-            let mut b = 0;
-            let mut c = 0;
-            let mut d = 0;
-            for n in 0..2000 {
-                let prev = (s % 10) as i32;
-                s = next(s);
-                let ones = (s % 10) as i32;
-                let change = ones - prev;
-                a = b;
-                b = c;
-                c = d;
-                d = change;
-                if n >= 3 && !change_map.contains_key(&(a, b, c, d)) {
-                    change_map.insert((a, b, c, d), ones);
-                }
-            }
-            change_map
-        })
-        .collect::<Vec<_>>();
-
-    let mut max = 0;
-    for a in -9..10 {
-        for b in -9..10 {
-            for c in -9..10 {
-                for d in -9..10 {
-                    let sum = change_maps
-                        .iter()
-                        .filter_map(|m| m.get(&(a, b, c, d)))
-                        .sum::<i32>();
-                    max = max.max(sum);
-                }
+    let mut change_map = BTreeMap::new();
+    secrets.for_each(|s| {
+        let mut seen = BTreeSet::new();
+        let mut s = s;
+        let mut a = 0;
+        let mut b = 0;
+        let mut c = 0;
+        let mut d = 0;
+        for n in 0..2000 {
+            let prev = (s % 10) as i32;
+            s = next(s);
+            let ones = (s % 10) as i32;
+            let change = ones - prev;
+            a = b;
+            b = c;
+            c = d;
+            d = change;
+            if n >= 3 && !seen.contains(&(a, b, c, d)) {
+                seen.insert((a, b, c, d));
+                change_map
+                    .entry((a, b, c, d))
+                    .and_modify(|total| *total += ones)
+                    .or_insert(ones);
             }
         }
-    }
-    max
+    });
+
+    *change_map.values().max().unwrap()
 }
 
 #[cfg(test)]
